4655da75a3b390a8280fe771565fe9f605da49ca
Zeyu-Li <zeyu.li23@gmail.com>
2021-12-31 13:53:43 -0600

    rendered triangles

--- openGL.cpp
+++ openGL.cpp
@@ -7,6 +7,20 @@
 #include <GLFW/glfw3.h>
 
 typedef long long ll;
+// Vertex Shader source code
+const char* vertexShaderSource = "#version 330 core\n"
+"layout (location = 0) in vec3 aPos;\n"
+"void main()\n"
+"{\n"
+"   gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
+"}\0";
+//Fragment Shader source code
+const char* fragmentShaderSource = "#version 330 core\n"
+"out vec4 FragColor;\n"
+"void main()\n"
+"{\n"
+"   FragColor = vec4(0.9f, 0.9f, 0.09f, 1.0f);\n"
+"}\n\0";
 
 int main()
 {
@@ -17,6 +31,13 @@ int main()
     glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
     glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+    // triangle object
+    GLfloat vertices[] = {
+        -0.5f, -0.5f * float(sqrt(3)) / 3, 0.0f, 
+        0.5f, -0.5f * float(sqrt(3)) / 3, 0.0f, 
+        0.0f, 1.0f * float(sqrt(3)) / 3, 0.0f
+    };
 
     // window height and width set to the same at 800px
     int height, width;
@@ -37,13 +58,72 @@ int main()
     // create window instance
     glfwMakeContextCurrent(window);
 
+    // update window screen to black
+    gladLoadGL();
+
+    // render view has a shown and buffer loading in the background
+    // we want to swap the buffer such that the buffer content gets displayed
+    glViewport(0, 0, height, width);
+
+    // create shader and pre compile shader
+    GLuint vertexShader = glCreateShader(GL_VERTEX_SHADER);
+    glShaderSource(vertexShader, 1, &vertexShaderSource, NULL);
+    glCompileShader(vertexShader);
+
+    GLuint fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
+    glShaderSource(fragmentShader, 1, &fragmentShaderSource, NULL);
+    glCompileShader(fragmentShader);
+
+    // attach shader to program
+    GLuint shaderProgram = glCreateProgram();
+    glAttachShader(shaderProgram, vertexShader);
+    glAttachShader(shaderProgram, fragmentShader);
+    glLinkProgram(shaderProgram);
+
+    // clean up
+    glDeleteShader(vertexShader);
+    glDeleteShader(fragmentShader);
+
+    // store shape in buffer
+    GLuint VBO, VAO;
+    glGenVertexArrays(1, &VAO);
+    glGenBuffers(1, &VBO);
+
+    glBindVertexArray(VAO);
+    glBindBuffer(GL_ARRAY_BUFFER, VBO);
+
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+    glEnableVertexAttribArray(0);
+
+    // bind both the VBO and VAO to 0 so that we don't accidentally modify the VAO and VBO we created
+    glBindBuffer(GL_ARRAY_BUFFER, 0);
+    glBindVertexArray(0);
+
     // main while loop
     while (!glfwWindowShouldClose(window)) {
+        // clear for every frame
+        glClearColor(0.18f, 0.18f, 0.25f, 1.0f);
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        // run shader
+        glUseProgram(shaderProgram);
+        glBindVertexArray(VAO);
+        glDrawArrays(GL_TRIANGLES, 0, 3);
+        // swap the buffers on each run
+        glfwSwapBuffers(window);
+
         // runner
         glfwPollEvents();
     }
 
-    // clean up
+    /* clean up */
+    // gl objects
+    glDeleteVertexArrays(1, &VAO);
+    glDeleteBuffers(1, &VBO);
+    glDeleteProgram(shaderProgram);
+    // window instance
     glfwDestroyWindow(window);
     glfwTerminate();
 
